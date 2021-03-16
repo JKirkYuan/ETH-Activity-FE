74f66d6845beff631d4da8ce76c704fae8ecac4b
JKirkYuan <yuanjt810@gmail.com>
2021-03-16 14:31:53 -0600

    Init project

---
+++ src/components/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#c3f2e3',
+      main: '#92bfb1',
+      dark: '#638f82',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#d8d4d4',
+      dark: '#a7a3a3',
+      contrastText: '#000000',
+    },
+  },
+})
